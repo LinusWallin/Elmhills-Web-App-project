3974a6cb66430d42812597cefa158a82035dfcd1
Linus Wallin <linuswallin@live.se>
2024-03-12 13:11:31 +0100

    feat: Button added to the navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,24 @@
 import React, {useState} from 'react';
 import { Link } from 'react-router-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import './Navbar.css'
+import { Button } from './Button';
 
 function Navbar() {
   const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
+
   const handleClick = () => setClick(!click);
   const closeMobileMenu = () => setClick(false);
+
+  const showButton = () => {
+    if(window.innerWidth <= 960) {
+      setButton(false);
+    } else {
+      setButton(true);
+    }
+  }
+
+  window.addEventListener('resize', showButton);
 
   return (
     <>
@@ -38,7 +51,13 @@ function Navbar() {
                 Våra Hundar
               </Link>
             </li>
+            <li className='nav-item'>
+              <Link to='/intresseanmalan' className='nav-links-mobile' onClick={closeMobileMenu}>
+                Intresseanmälan
+              </Link>
+            </li>
           </ul>
+          {button && <Button buttonStyle='btn--outline'>Intresseanmälan</Button>}
         </div>
       </nav>
     </>
