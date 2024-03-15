6beaa8cc8e9eabc9b6876dc8e8008286826a5a99
Linus Wallin <linuswallin@live.se>
2024-03-15 17:52:54 +0100

    feat: new text box for home page Created a text box for the home page with a welcome message.

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -3,7 +3,13 @@ import React from 'react'
 function Home() {
   return (
     <div className='home'>
-      Home
+      <div className='welcome'>
+        <h1>Välkommen till Elmhills kennel!</h1>
+        <p>
+          Elmhills kennel är en SKK godkänd kennel som bedriver en småskalig uppfödning
+          av flerfärgade dvärgpudlar strax utanför Västerås.
+        </p>
+      </div>
     </div>
   )
 }
