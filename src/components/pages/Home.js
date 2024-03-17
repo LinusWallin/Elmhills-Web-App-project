88e85665187633a1d135f58e4a521b9cce0773de
Linus Wallin <73783914+LinusWallin@users.noreply.github.com>
2024-03-17 15:24:36 +0100

    2 homepage (#8)
    * chore: relocation of files
    
    * build: added react-router-dom to packages
    
    * feat: Adds four new routes
    
    Adds four new routes and a hamburger icon which changes into a cross
    when clicked.
    
    * feat: Button added to the navbar
    
    * Refactor: removes button from the router display
    
    Removes the button from the router display and also changes the
    style of the router display.
    
    * feat: created home page that displays the text "Home"
    
    * chore: added image
    
    * feat: new text box for home page
    Created a text box for the home page with a welcome message.
    
    * changed layout of the website

---
+++ src/components/pages/Home.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div className='home'>
+      <div className='welcome'>
+        <h1>Välkommen till Elmhills kennel!</h1>
+        <p>
+          Elmhills kennel är en SKK godkänd kennel som bedriver en småskalig uppfödning
+          av flerfärgade dvärgpudlar strax utanför Västerås.
+        </p>
+        <p className='construction'>
+          Hemsidan är under utveckling!
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default Home
