7d0cd8e66e43fa3e0f2ab03625189f238e59df41
Linus Wallin <linuswallin@live.se>
2024-03-17 15:35:30 +0100

    feat: creates simple about page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/pages/Home';
+import About from './components/pages/About';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Navbar />
         <Routes>
           <Route path='/' exact Component={Home}/>
+          <Route path='/om-oss' exact Component={About}/>
         </Routes>
       </Router>
     </>
---
+++ src/components/pages/About.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function About() {
+  return (
+    <div className='about'>
+      <div className='welcome'>
+        <p className='construction'>
+          Hemsidan är under utveckling!
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default About
