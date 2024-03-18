1cc690184ac10f1db4b0c68fa0144e9d76c61d53
Linus Wallin <linuswallin@live.se>
2024-03-18 16:55:09 +0100

    feat: adds route to the puppies component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/pages/Home';
 import About from './components/pages/About';
+import Puppies from './components/pages/Puppies';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path='/' exact Component={Home}/>
           <Route path='/om-oss' exact Component={About}/>
+          <Route path='/valpar' exact Component={Puppies}/>
         </Routes>
       </Router>
     </>
