dc8d8a2d7451fa346fa6e64d8a5cd946765a387f
Linus Wallin <linuswallin@live.se>
2024-03-18 16:39:26 +0100

    feat: implements footer to the two current pages

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,14 +1,18 @@
 import React from 'react'
+import Footer from '../Footer'
 
 function About() {
   return (
-    <div className='about'>
-      <div className='welcome'>
-        <p className='construction'>
-          Hemsidan är under utveckling!
-        </p>
+    <>
+      <div className='about'>
+        <div className='welcome'>
+          <p className='construction'>
+            Hemsidan är under utveckling!
+          </p>
+        </div>
       </div>
-    </div>
+      <Footer/>
+    </>
   )
 }
 
