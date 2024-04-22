ef821ff742bad520e268a2b428d735149017272a
Linus Wallin <linuswallin@live.se>
2024-04-22 17:28:52 +0200

    feat: expands sub page for puppies

--- src/components/pages/Previous.js
+++ src/components/pages/Previous.js
@@ -4,25 +4,27 @@ import Footer from '../Footer'
 function Previous() {
   return (
     <>
-        <div className='text-container-left text-container'>
-          <p className='rubric-1'>
-            Valpar 2022
-          </p>
-          <p className='rubric-2'>
-          Den 25:e mars 2022 tittade den första valpen ut. I nio 
-          veckor hade vi tålmodigt väntat på att få se vad som 
-          fanns i magen. Spänningen var olidlig. Vilka färger 
-          skulle valparna ha? Hur många skulle det vara? Kön? 
-          Många frågor susade genom tankarna, men vips så var alla 
-          fyra småttingar ute och vi kunde beundra våra första 
-          egenuppfödda valpar. Det blev många nätter med minimal 
-          sömn till en början, men sedan blev jobbet smått lättare 
-          en stund till att sedan vara full rulle igen. Rätt vad 
-          det var så hade alla valpar flyttat till sina egna hem 
-          och det blev tomt i vårt hus.
-          </p>
-        </div>
-        <Footer/>
+      <div className='prev'>
+      </div>
+      <div className='text-container-left text-container'>
+        <p className='rubric-1'>
+          Valpar 2022
+        </p>
+        <p className='rubric-2'>
+        Den 25:e mars 2022 tittade den första valpen ut. I nio 
+        veckor hade vi tålmodigt väntat på att få se vad som 
+        fanns i magen. Spänningen var olidlig. Vilka färger 
+        skulle valparna ha? Hur många skulle det vara? Kön? 
+        Många frågor susade genom tankarna, men vips så var alla 
+        fyra småttingar ute och vi kunde beundra våra första 
+        egenuppfödda valpar. Det blev många nätter med minimal 
+        sömn till en början, men sedan blev jobbet smått lättare 
+        en stund till att sedan vara full rulle igen. Rätt vad 
+        det var så hade alla valpar flyttat till sina egna hem 
+        och det blev tomt i vårt hus.
+        </p>
+      </div>
+      <Footer/>
     </>
   )
 }
